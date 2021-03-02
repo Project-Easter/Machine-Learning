ad95062e0cb0ffeec9a824496572ef50af67511c
ankitoscar <ankitoscar911@gmail.com>
2021-03-02 14:56:08 +0530

    fixed some bugs

--- flutter/model_1/script.py
+++ flutter/model_1/script.py
@@ -2,30 +2,32 @@
 import pandas as pd 
 import numpy as np 
 from sklearn.metrics.pairwise import cosine_similarity 
-from sklearn.feature_extraction import CountVectorizer, TfidfVectorizer 
+from sklearn.feature_extraction.text import TfidfVectorizer 
 
 class recommendation:
+
     def __init__(self):
         """
         Constructor for recommendation class
         """
+        self.preprocessing()
 
     def preprocessing(self):
-        self.df = pd.read_csv('model1_data.csv')
+        self.df = pd.read_csv('flutter\model_1\model1_data.csv')
         self.df = self.df.sample(frac=1)
         self.df.drop('Unnamed: 0', axis=1, inplace=True)
         self.df.dropna(inplace=True)
-        self.book_data = self.df.loc[self.df['Genre'] == genre]
-        self.book_data.reset_index(level = 0, inplace = True)
-        self.indices = pd.Series(self.book_data.index, index = self.book_data['Title'])
 
-        self.tf = TfidfVectorizer(analyzer = 'word', ngram_range = (2,2), min_df = 1, stop_words = 'english')
-        self.tfidf_mat = self.tf.fit_transform(self.book_data['Title'])
+    def recommend_with_genre(self,title,genre):
+        book_data = self.df.loc[self.df['Genre'] == genre]
+        book_data.reset_index(level = 0, inplace = True)
+        indices = pd.Series(book_data.index, index = book_data['Title'])
 
-    def recommend_with_genre(title,genre,self.tfidf_mat):
-        sg = cosine_similarity(self.tfidf_mat, self.tfidf_mat)
+        tf = TfidfVectorizer(analyzer = 'word', ngram_range = (2,2), min_df = 1, stop_words = 'english')
+        tfidf_mat = tf.fit_transform(book_data['Title'])
+        sg = cosine_similarity(tfidf_mat, tfidf_mat)
 
-        idx = self.indices[title]
+        idx = indices[title]
 
         sig = list(enumerate(sg[idx]))
 
@@ -34,12 +36,15 @@ class recommendation:
 
         return sig
 
-    def get_title_recommedations(title,genre,self.tfidf_mat):
+    def get_title_recommendations(self,title,genre):
+        book_data = self.df.loc[self.df['Genre'] == genre]
+        book_data.reset_index(level = 0, inplace = True)
+
         recoms  = self.recommend_with_genre(title,genre)
 
         book_indices = [i[0] for i in recoms]
 
         recommendations = []
         for i in book_indices:
-            recommendations.append(self.book_data['Title'].loc[i])
+            recommendations.append(book_data['Title'].loc[i])
         return recommendations
