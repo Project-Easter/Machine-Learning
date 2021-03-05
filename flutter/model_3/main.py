e2e706e81a6046172e6c68f869fe84f112e1a5b1
ankitoscar <ankitoscar911@gmail.com>
2021-03-05 22:08:32 +0530

    added get_title() in api

--- flutter/model_3/main.py
+++ flutter/model_3/main.py
@@ -12,5 +12,20 @@ def recommend_with_isbn(isbn):
     recommend_list = re.get_recommedations(isbn)
     return jsonify(recommend_list)
 
+@app.route('/book_title/<string:title>')
+def get_title(title):
+    re = recommendation()
+    matches = re.matching(title)
+    match_list = {}
+    i = int(0)
+    for match in matches:
+        book_number = "Book_" + str(i+1)
+        title = match[0]
+        isbn = match[1]
+        match_list[book_number] = {'Title' : title, 'ISBN' : isbn}
+        i += 1 
+    
+    return jsonify(match_list)
+    
 if __name__ == "__main__":
     app.run(debug = True)
